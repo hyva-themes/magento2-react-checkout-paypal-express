5c1b101db88e21628700883d4721e1a51b113fbe
ilanbaron <baron.ilan@gmail.com>
2021-06-07 12:33:07 -0300

    changes

---
+++ src/hooks/usePaypalExpressAppContext.js
@@ -0,0 +1,12 @@
+import { useContext } from 'react';
+
+import AppContext from '../../../../context/App/AppContext';
+
+export default function usePaypalExpressCartContext() {
+  const [, { setErrorMessage, setPageLoader }] = useContext(AppContext);
+
+  return {
+    setErrorMessage,
+    setPageLoader,
+  };
+}
